af57e8be7a2173744b4e7bff1f2bcf99a26a441d
DavidBeugelsdijk <112309@emmauscollege.nl>
2021-06-12 18:41:03 +0000

    comments af, game klaar
    Signed-off-by: DavidBeugelsdijk <112309@emmauscollege.nl>

--- script.js
+++ script.js
@@ -99,47 +99,47 @@ var tekenBlokje = function() {
     for (var i = 0; i < 3; i = i + 1) { // zodat de code voor alle drie de blokken wordt uitgevoerd
         for (var k = 0; k < blokWidth[i]; k = k + 70) { // deze gigantische for-loop met absurd veel if-jes zorgt er voor dat ieder blokje de juiste graphics heeft.
             for(var l = 0; l < blokHeight[i]; l = l + 70) {
-                if(k === 0 && k + 70 === blokWidth[i] && l === 0 && l + 70 === blokHeight[i]) {
+                if(k === 0 && k + 70 === blokWidth[i] && l === 0 && l + 70 === blokHeight[i]) { // blokje van 70 × 70.
                     image(imgGroundSingleTile, blokX[i], blokY[i], 70, 70);
-                } else if (k === 0 && k + 70 === blokWidth[i]) {
-                    if(l === 0) {
+                } else if (k === 0 && k + 70 === blokWidth[i]) { // blokje van 70 × n met n is niet 70.
+                    if(l === 0) { // bovenste deel
                         image(imgGroundSingleTileVerticaalBoven, blokX[i], blokY[i] + l + 1, 70, 70);
-                    } else if (l + 70 === blokHeight[i]) {
+                    } else if (l + 70 === blokHeight[i]) { // onderste deel
                         image(imgGroundSingleTileVerticaalOnder, blokX[i], blokY[i] + l + 1, 70, 70);
-                    } else {
+                    } else { // overig deel in hoogte
                         image(imgGroundSingleTileVerticaalMidden, blokX[i], blokY[i] + l + 1, 70, 70);
                     }
-                } else if (l === 0 && l + 70 === blokHeight[i]) {
-                    if (k === 0) {
+                } else if (l === 0 && l + 70 === blokHeight[i]) {// blokje van n × 70 met n is niet 70. 
+                    if (k === 0) { // linker deel
                         image(imgGroundSingleTileHorizontaalLinks, blokX[i] + k + 1, blokY[i], 70, 70);
-                    } else if (k + 70 === blokWidth[i]) {
+                    } else if (k + 70 === blokWidth[i]) { // rechter deel
                         image(imgGroundSingleTileHorizontaalRechts, blokX[i] + k + 1, blokY[i], 70, 70);
-                    } else {
+                    } else { // overig deel in breedte
                         image(imgGroundSingleTileHorizontaalMidden, blokX[i] + k + 1, blokY[i], 70, 70);
                     }
-                } else {
-                    if (k === 0) {
-                        if(l === 0) {
+                } else { // overige blokjes
+                    if (k === 0) { // linker deel
+                        if(l === 0) { // bovenste deel
                             image(imgGroundLinksBoven, blokX[i] + k + 1, blokY[i] + l + 1, 70, 70);
-                        } else if (l + 70 === blokHeight[i]) {
+                        } else if (l + 70 === blokHeight[i]) { // onderste deel
                             image(imgGroundLinksOnder, blokX[i] + k + 1, blokY[i] + l + 1, 70, 70);
-                        } else {
+                        } else { // overig deel in hoogte
                             image(imgGroundLinksMidden, blokX[i] + k + 1, blokY[i] + l + 1, 70, 70);
                         }
-                    } else if (k + 70 === blokWidth[i]) {
-                        if(l === 0) {
+                    } else if (k + 70 === blokWidth[i]) { // rechter deel
+                        if(l === 0) { // bovenste deel
                             image(imgGroundRechtsBoven, blokX[i] + k + 1, blokY[i] + l + 1, 70, 70);
-                        } else if (l + 70 === blokHeight[i]) {
+                        } else if (l + 70 === blokHeight[i]) { // onderste deel
                             image(imgGroundRechtsOnder, blokX[i] + k + 1, blokY[i] + l + 1, 70, 70);
-                        } else {
+                        } else { // overig deel in hoogte
                             image(imgGroundRechtsMidden, blokX[i] + k + 1, blokY[i] + l + 1, 70, 70);
                         }
-                    } else {
-                        if(l === 0) {
+                    } else { // overig deel in breedte
+                        if(l === 0) { // bovenste deel
                             image(imgGroundMiddenBoven, blokX[i] + k + 1, blokY[i] + l + 1, 70, 70);
-                        } else if (l + 70 === blokHeight[i]) {
+                        } else if (l + 70 === blokHeight[i]) { // onderste deel
                             image(imgGroundMiddenOnder, blokX[i] + k + 1, blokY[i] + l + 1, 70, 70);
-                        } else {
+                        } else { // overig deel in hoogte
                             image(imgGroundMidden, blokX[i] + k + 1, blokY[i] + l + 1, 70, 70);
                         }
                     }
@@ -159,7 +159,7 @@ var tekenBlokje = function() {
             blokX[i] = 1280;
             blokY[i] = round(random(450, 600));
             blokWidth[i] = blokAfmetingPool[round(random(-0.5, 3.5))]; // gaat van 0.5 tot 3.5 zodat alle mogelijke waardes (0-3) kunnen worden gekozen.
-            blokHeight[i] = blokAfmetingPool[round(random(-0.5, 2.5))]; // deze gaat tot 2.5 zodat de blokken niet te hoog kunne worden om overheen te springen.
+            blokHeight[i] = blokAfmetingPool[round(random(-0.5, 2.5))]; // deze gaat tot 2.5 zodat de blokken niet te hoog om overheen te springen kunnen worden .
             while (blokY[i] + blokHeight[i] > 680) { // zorgt er voor dat een blok een nieuwe hoogte krijgt, als die een hoogte had waarmee die onder de grond uitkwam.
                 blokHeight[i] = blokAfmetingPool[round(random(-0.5, 1.5))];
             }
@@ -290,9 +290,9 @@ var gameReset = function () {
             blokX[i] = 2060;
         }
         blokY[i] = round(random(450, 600));
-        blokWidth[i] = blokAfmetingPool[round(random(-0.5, 3.5))];
-        blokHeight[i] = blokAfmetingPool[round(random(-0.5, 2.5))];
-        while (blokY[i] + blokHeight[i] > 680) {
+        blokWidth[i] = blokAfmetingPool[round(random(-0.5, 3.5))]; // gaat van 0.5 tot 3.5 zodat alle mogelijke waardes (0-3) kunnen worden gekozen.
+        blokHeight[i] = blokAfmetingPool[round(random(-0.5, 2.5))]; // deze gaat tot 2.5 zodat de blokken niet te hoog om overheen te springen kunnen worden .
+        while (blokY[i] + blokHeight[i] > 680) { // zorgt er voor dat een blok een nieuwe hoogte krijgt, als die een hoogte had waarmee die onder de grond uitkwam.
             blokHeight[i] = blokAfmetingPool[round(random(-0.5, 1.5))];
         }
     }
@@ -315,7 +315,7 @@ var tekenVerliesScherm = function () {
         fill('black');
         textSize(40);
         text(buttonText[i], 230 * i + 450 , 477);
-        if (mouseX > 270 * i + 380 && mouseX < 270 * i + 630 && mouseY > 415 && mouseY < 515) {
+        if (mouseX > 270 * i + 380 && mouseX < 270 * i + 630 && mouseY > 415 && mouseY < 515) { // zorgt ervoor dat de inhoud van de knop een beetje donkerder wordt als je er met je muis boven zit.
            fill("rgba(0, 0, 0, 0.2)"); 
         } else {
             noFill();
@@ -324,6 +324,10 @@ var tekenVerliesScherm = function () {
     }
 }
 
+
+/**
+ * start het spel
+ */
 var checkStart = function () {
     if (mouseX > 380 && mouseX < 630 && mouseY > 415 && mouseY < 515 && mouseIsClicked === true) {
         gameReset();
@@ -331,9 +335,13 @@ var checkStart = function () {
     }
 }
 
+
+/**
+ * Tekent het beginscherm
+ */
 var tekenHomeScreen = function () {
     image(imgBackground, 0, 0, width, height);
-    fill('rgb(255, 127, 0)');
+    fill('rgb(255, 127, 0)'); // de kleur is oranje
     textSize(80);
     text('Jumpin\' Jack', 410, 100)
     fill('black');
@@ -346,7 +354,7 @@ var tekenHomeScreen = function () {
         fill('black');
         textSize(40);
         text(buttonText[i], 230 * i + 470 , 477);
-        if (mouseX > 270 * i + 380 && mouseX < 270 * i + 630 && mouseY > 415 && mouseY < 515) {
+        if (mouseX > 270 * i + 380 && mouseX < 270 * i + 630 && mouseY > 415 && mouseY < 515) { // zorgt ervoor dat de inhoud van de knop een beetje donkerder wordt als je er met je muis boven zit.
             fill("rgba(0, 0, 0, 0.2)"); 
         } else {
             noFill();
@@ -355,21 +363,29 @@ var tekenHomeScreen = function () {
     }
 } 
 
+
+/**
+ * Controleert of er een nieuwe highscore is en geeft die door
+ */
 var checkNewHighscore = function () {
     if (score > highscore) {
         highscore = score;
     }
 }
 
+
+/**
+ * Controleert of het scherm met de contoles van het spel getekend moet worden en tekent het als het moet.
+ */
 var checkControls = function () {
-    if (mouseX > 650 && mouseX < 900 && mouseY > 415 && mouseY < 515 && mouseIsClicked === true && spelStatus === UITLEG) {
+    if (mouseX > 650 && mouseX < 900 && mouseY > 415 && mouseY < 515 && mouseIsClicked === true && spelStatus === UITLEG) { // als je op de knop klikt wordt het scherm met de controles getekend.
         controlScherm = true;
     }
     if (controlScherm === true) {
-        if (mouseX > 1137 && mouseX < 1150 && mouseY > 250 && mouseY < 265) {
+        if (mouseX > 1137 && mouseX < 1150 && mouseY > 250 && mouseY < 265) { // als je boven het kruisje van het venster zit met je muis, wordt die rood.
             fill('red');
             rect(1137, 250, 14, 14);
-            if (mouseIsClicked === true) {
+            if (mouseIsClicked === true) { // als je op het kruisje klikt, gaat het venster weg.
                 controlScherm = false;
             }
         }
@@ -387,39 +403,61 @@ var checkControls = function () {
     }
 }
 
+
+/**
+ * Controleert of je naar het beginscherm gaat
+ */
 var checkMainMenu = function () {
-    if (mouseX > 650 && mouseX < 900 && mouseY > 415 && mouseY < 665 && mouseIsClicked === true && spelStatus === GAMEOVER) {
+    if (mouseX > 650 && mouseX < 900 && mouseY > 415 && mouseY < 665 && mouseIsClicked === true && spelStatus === GAMEOVER) { // als je op de knop klikt ga je naar het beginscherm
         return true;
     }
 }
 
+
+/**
+ * Zorgt ervoor dat je een enkele output krijgt bij het klikken
+ * Was nodig zodat als je op een knop drukte en daardoor naar een ander scherm ging, je niet meteen ook daar op een knop drukt
+ */
 var checkMouseIsClicked = function () {
-    if(mouseIsPressed && mouseIsClicked === false && mouseWasClicked === false) {
+    if(mouseIsPressed && mouseIsClicked === false && mouseWasClicked === false) { // als de muis net wordt ingedrukt, wordt dat doorgegeven.
         mouseIsClicked = true;
-    } else if (mouseIsPressed) {
+    } else if (mouseIsPressed) { // als de muis langer dan een input (0,02 secondes) wordt ingedrukt, wordt er doorgegeven dat de muis niet meer een output mag geven.
         mouseIsClicked = false;
         mouseWasClicked = true;
-    } else {
+    } else { // als de muis niet meer is ingedrukt, wordt dat doorgegeven. Er moest hier trouwens 'mouseIsClicked = false;' staan, omdat het anders niet werkte en ik weet niet helemaal waarom.
         mouseIsClicked = false;
         mouseWasClicked = false;
     }
 }
 
+
+/**
+ * Kijkt welk blok het dichtsbij de speler is en maakt dit het actieve blok
+ * ik wou deze code compacter maken maar dan had ik een formule nodig die bij input: 1 output: 2 gaf, bij input: 2 output: 3 gaf en bij input: 3 output: 1 gaf.
+ * ik had het een beetje korter kunnen maken, maar dan staat er een kort for-loop-je in de code en het maakt de code nog onduidelijker
+ */
 var checkBlokDichtstbij = function() {
     afstandBlok0 = blokX[0] - (spelerX + spelerWidth);
     afstandBlok1 = blokX[1] - (spelerX + spelerWidth);
     afstandBlok2 = blokX[2] - (spelerX + spelerWidth);
-    if (afstandBlok0 < afstandBlok1 && afstandBlok0 < afstandBlok2 && afstandBlok0 >= -1 * (blokWidth[0] + spelerWidth)) {
+    if ((afstandBlok0 < afstandBlok1 || afstandBlok1 < -1 * (blokWidth[1] + spelerWidth)) && (afstandBlok0 < afstandBlok2 || afstandBlok2 < -1 * (blokWidth[2] + spelerWidth)) && afstandBlok0 >= -1 * (blokWidth[0] + spelerWidth)) { // als blok 1 het dichtstbij en niet voorbij de speler is, wordt dat het actieve blok. Ook gebeurt dit als de blokken voor dit voorbij de speler zijn.
         actiefBlok = 0;
     }
-    if ((afstandBlok1 < afstandBlok0 || afstandBlok0 <= -1 * (blokWidth[0] + spelerWidth)) && afstandBlok1 < afstandBlok2 && afstandBlok1 >= -1 * (blokWidth[1] + spelerWidth)) {
+    if ((afstandBlok1 < afstandBlok0 || afstandBlok0 < -1 * (blokWidth[0] + spelerWidth)) && (afstandBlok1 < afstandBlok2 || afstandBlok2 < -1 * (blokWidth[2] + spelerWidth)) && afstandBlok1 >= -1 * (blokWidth[1] + spelerWidth)) { // als blok 1 het dichtstbij en niet voorbij de speler is, wordt dat het actieve blok. Ook gebeurt dit als de blokken voor dit voorbij de speler zijn.
         actiefBlok = 1;
     }
-    if (afstandBlok2 < afstandBlok0 && afstandBlok2 < afstandBlok1 && afstandBlok2 >= -1 * (blokWidth[2] + spelerWidth)) {
+    if ((afstandBlok2 < afstandBlok0 || afstandBlok0 < -1 * (blokWidth[0] + spelerWidth)) && (afstandBlok2 < afstandBlok1 || afstandBlok1 < -1 * (blokWidth[1] + spelerWidth)) && afstandBlok2 >= -1 * (blokWidth[2] + spelerWidth)) { // als blok 1 het dichtstbij en niet voorbij de speler is, wordt dat het actieve blok. Ook gebeurt dit als de blokken voor dit voorbij de speler zijn.
         actiefBlok = 2;
     }
 }
 
+
+/**
+ * preload
+ * de code in deze functie wordt één keer uitgevoerd door
+ * de p5 library, voordat het spel geladen is in de browser
+ * in dit geval gebruikt om alle afbeeldingen te laden
+ */
 function preload() {
     imgJack = loadImage('./images/jack_staan.gif');
     imgJackSlide = loadImage('./images/jack_slide.gif');
